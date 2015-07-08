95a42737b81ec30d03e5251d046e416150e1e740
wanghongmeng <wanghongmeng@live.cn>
2015-07-08 18:12:06 +0800

    修改广东南粤银行抓取

--- src/main/java/cn/com/fero/tlc/spider/quartz/job/NYYHNYEJob.java
+++ src/main/java/cn/com/fero/tlc/spider/quartz/job/NYYHNYEJob.java
@@ -6,7 +6,9 @@ import cn.com.fero.tlc.spider.quartz.TLCSpiderJob;
 import cn.com.fero.tlc.spider.util.DateFormatUtil;
 import cn.com.fero.tlc.spider.util.JsonUtil;
 import cn.com.fero.tlc.spider.util.LoggerUtil;
+import cn.com.fero.tlc.spider.util.PropertiesUtil;
 import cn.com.fero.tlc.spider.vo.NYYHNYE;
+import cn.com.fero.tlc.spider.vo.RDNSYHERJZ;
 import cn.com.fero.tlc.spider.vo.TransObject;
 import org.apache.commons.lang3.StringUtils;
 import org.quartz.JobExecutionContext;
@@ -23,65 +25,81 @@ import java.util.Map;
  */
 //广东南粤银行南粤E+抓取
 public class NYYHNYEJob extends TLCSpiderJob {
-    private static final String URL_PRODUCT_LIST = "https://one.gdnybank.com/directbank/erongProjectsList.do";
+    //detail: https://one.gdnybank.com/pages/er_product_detail.html
 
-    public void execute(JobExecutionContext context) throws JobExecutionException {
-        LoggerUtil.getLogger().info("开始抓取广东南粤银行南粤E+");
+    private static final String URL_PRODUCT_LIST = PropertiesUtil.getResource("tlc.spider.nyyhnye.url.list");
+    private static final String SID = PropertiesUtil.getResource("tlc.spider.nyyhnye.sid");
+    private static final String TOKEN = PropertiesUtil.getResource("tlc.spider.nyyhnye.token");
+    private static final String JOB_TITLE = PropertiesUtil.getResource("tlc.spider.nyyhnye.title");
 
+    @Override
+    public Map<String, String> constructSystemParam() {
+        Map<String, String> param = new HashMap();
+        param.put(TLCSpiderConstants.SPIDER_PARAM_STATUS_NAME, TLCSpiderConstants.SPIDER_PARAM_STATUS_SUCCESS_CODE);
+        param.put(TLCSpiderConstants.SPIDER_PARAM_SID, SID);
+        param.put(TLCSpiderConstants.SPIDER_PARAM_TOKEN, TOKEN);
+        param.put(TLCSpiderConstants.SPIDER_CONST_JOB_TITLE, JOB_TITLE);
+        param.put(TLCSpiderConstants.SPIDER_PARAM_PAGE_NAME, "page");
+        return param;
+    }
+
+    @Override
+    public Map<String, String> constructSpiderParam() {
         Map<String, String> param = new HashMap();
         param.put("timeLimitType", "0");
         param.put("page", "0");
         param.put("pagesize", TLCSpiderConstants.SPIDER_PAGE_SIZE_GET);
+        return param;
+    }
 
+    @Override
+    public int getTotalPage(Map<String, String> param) {
         String countContent = TLCSpiderRequest.post(URL_PRODUCT_LIST, param);
         String totalCount = JsonUtil.getString(countContent, "count");
+        return Integer.parseInt(totalCount);
+    }
 
-        param.put("pagesize", totalCount);
+    @Override
+    public List<TransObject> getSpiderDataList(Map<String, String> param) {
         String productContent = TLCSpiderRequest.post(URL_PRODUCT_LIST, param);
-        List<NYYHNYE> nyyhnyeList = JsonUtil.json2Array(productContent, "projList", NYYHNYE.class);
+        List<NYYHNYE> productList = JsonUtil.json2Array(productContent, "projList", NYYHNYE.class);
+
         List<TransObject> transObjectList = new ArrayList();
+        for(NYYHNYE product : productList) {
+            TransObject transObject = convertToTransObject(product);
+            transObjectList.add(transObject);
+        }
 
+        return transObjectList;
+    }
 
-//        "sumAmt": "23900",
-//        "leftAmt": "476100",
-//        "buyFlag": "1",
-//        "limitNum": "3",
-//        "setupDate": "20150626",
-//        "singleSum": "100",
-//        "recomType": "0",
-//        "leftTime": "-1天-3小时-53分-13秒",
-//        "investFlag": "1",
-//        "acceptor": "",
-//        "startFlag": "1",
-//        "setlBatchDate": "20150805",
+    private TransObject convertToTransObject(NYYHNYE product) {
+        TransObject transObject = new TransObject();
+        transObject.setFinancingId(product.getProdCode());
+        transObject.setProjectCode(product.getProjCode());
+        transObject.setProjectName(product.getProjName());
+        transObject.setAmount(product.getProductSize());
+        transObject.setDuration(product.getLimitType());
+        transObject.setInvestmentInterest(Double.parseDouble(product.getYieldRate()) * 100 + "%");
+        transObject.setRealProgress(Double.parseDouble(product.getBuyPercent()) * 100 + "%");
+        transObject.setProgress(Double.parseDouble(product.getBuyPercent()) * 100 + "%");
+        if (StringUtils.isNotEmpty(product.getPubStaDate()) && StringUtils.isNotEmpty(product.getPubStaTime())) {
+            transObject.setProjectBeginTime(DateFormatUtil.formatDateTime(TLCSpiderConstants.SPIDER_CONST_FORMAT_DATE_TIME, product.getPubStaDate(), product.getPubStaTime()));
+        } else if (StringUtils.isNotEmpty(product.getPubStaDate())) {
+            transObject.setProjectBeginTime(DateFormatUtil.formatDateTime(TLCSpiderConstants.SPIDER_CONST_FORMAT_DATA, product.getPubStaDate()));
+        }
+        if (StringUtils.isNotEmpty(product.getSellStaDate()) && StringUtils.isNotEmpty(product.getSellStaTime())) {
+            transObject.setValueBegin(DateFormatUtil.formatDateTime(TLCSpiderConstants.SPIDER_CONST_FORMAT_DATE_TIME, product.getSellStaDate(), product.getSellStaTime()));
+        } else if (StringUtils.isNotEmpty(product.getPubStaDate())) {
+            transObject.setValueBegin(DateFormatUtil.formatDateTime(TLCSpiderConstants.SPIDER_CONST_FORMAT_DATA, product.getSellStaDate()));
+        }
+        if (StringUtils.isNotEmpty(product.getSellEndDate()) && StringUtils.isNotEmpty(product.getSellEndTime())) {
+            transObject.setRepayBegin(DateFormatUtil.formatDateTime(TLCSpiderConstants.SPIDER_CONST_FORMAT_DATE_TIME, product.getSellEndDate(), product.getSellEndTime()));
+        } else if (StringUtils.isNotEmpty(product.getSellEndDate())) {
+            transObject.setRepayBegin(DateFormatUtil.formatDateTime(TLCSpiderConstants.SPIDER_CONST_FORMAT_DATA, product.getSellEndDate()));
+        }
 
-        for (NYYHNYE nyyhnye : nyyhnyeList) {
-            TransObject transObject = new TransObject();
-            transObject.setFinancingId(nyyhnye.getProdCode());
-            transObject.setProjectCode(nyyhnye.getProjCode());
-            transObject.setProjectName(nyyhnye.getProjName());
-            transObject.setAmount(nyyhnye.getProductSize());
-            transObject.setDuration(nyyhnye.getLimitType());
-            transObject.setInvestmentInterest(Double.parseDouble(nyyhnye.getYieldRate()) * 100 + "%");
-            transObject.setRealProgress(Double.parseDouble(nyyhnye.getBuyPercent()) * 100 + "%");
-            transObject.setProgress(Double.parseDouble(nyyhnye.getBuyPercent()) * 100 + "%");
-            if (StringUtils.isNotEmpty(nyyhnye.getPubStaDate()) && StringUtils.isNotEmpty(nyyhnye.getPubStaTime())) {
-                transObject.setProjectBeginTime(DateFormatUtil.formatDateTime(TLCSpiderConstants.SPIDER_CONST_FORMAT_DATE_TIME, nyyhnye.getPubStaDate(), nyyhnye.getPubStaTime()));
-            } else if (StringUtils.isNotEmpty(nyyhnye.getPubStaDate())) {
-                transObject.setProjectBeginTime(DateFormatUtil.formatDateTime(TLCSpiderConstants.SPIDER_CONST_FORMAT_DATA, nyyhnye.getPubStaDate()));
-            }
-            if (StringUtils.isNotEmpty(nyyhnye.getSellStaDate()) && StringUtils.isNotEmpty(nyyhnye.getSellStaTime())) {
-                transObject.setValueBegin(DateFormatUtil.formatDateTime(TLCSpiderConstants.SPIDER_CONST_FORMAT_DATE_TIME, nyyhnye.getSellStaDate(), nyyhnye.getSellStaTime()));
-            } else if (StringUtils.isNotEmpty(nyyhnye.getPubStaDate())) {
-                transObject.setValueBegin(DateFormatUtil.formatDateTime(TLCSpiderConstants.SPIDER_CONST_FORMAT_DATA, nyyhnye.getSellStaDate()));
-            }
-            if (StringUtils.isNotEmpty(nyyhnye.getSellEndDate()) && StringUtils.isNotEmpty(nyyhnye.getSellEndTime())) {
-                transObject.setRepayBegin(DateFormatUtil.formatDateTime(TLCSpiderConstants.SPIDER_CONST_FORMAT_DATE_TIME, nyyhnye.getSellEndDate(), nyyhnye.getSellEndTime()));
-            } else if (StringUtils.isNotEmpty(nyyhnye.getSellEndDate())) {
-                transObject.setRepayBegin(DateFormatUtil.formatDateTime(TLCSpiderConstants.SPIDER_CONST_FORMAT_DATA, nyyhnye.getSellEndDate()));
-            }
-
-            //TODO 转换
+        //TODO 转换
 //            financeValue.setPartsCount();
 //            financeValue.setBankInterest();
 //            financeValue.setRepayType();
@@ -112,9 +130,6 @@ public class NYYHNYEJob extends TLCSpiderJob {
 //            financeValue.setTitle();
 //            financeValue.setContent();
 //            financeValue.setIsLimitCount();
-            transObjectList.add(transObject);
-        }
-
-        print(transObjectList);
+        return transObject;
     }
 }
