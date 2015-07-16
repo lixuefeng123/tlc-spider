e7152219d0a69f1903b4701bc10115bb44b5d11f
wanghongmeng <wanghongmeng@live.cn>
2015-07-16 10:38:18 +0800

    修改启动/停止脚本

--- src/main/bin/shell/shutdown.sh
+++ src/main/bin/shell/shutdown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ${basedir}/conf/tlc-spider.conf
+source conf/tlc-spider.conf
 /bin/kill -9 `cat ${pid}`
 current_date=`date "+%Y-%m-%d %H:%M:%S"`
 echo "${current_date} shutdown tlc-spider by shutdown.sh" >>${log_path}/${log_name}.log
