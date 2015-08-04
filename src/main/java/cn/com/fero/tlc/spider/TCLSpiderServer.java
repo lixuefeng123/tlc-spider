7e87863d33141e1266e22cd1f0bf677189b357b8
wanghongmeng <wanghongmeng@live.cn>
2015-08-04 14:02:11 +0800

    打开schedule

--- src/main/java/cn/com/fero/tlc/spider/TCLSpiderServer.java
+++ src/main/java/cn/com/fero/tlc/spider/TCLSpiderServer.java
@@ -19,7 +19,7 @@ public final class TCLSpiderServer {
         try {
             TLCSpiderLoggerUtil.getLogger().info("加载spring配置文件");
             applicationContext = new ClassPathXmlApplicationContext("classpath*: spring-*.xml");
-//            p2pScheduler = initScheduler("tlcSpiderP2PScheduler");
+            p2pScheduler = initScheduler("tlcSpiderP2PScheduler");
             articleScheduler = initScheduler("tlcSpiderArticleScheduler");
         } catch (BeansException e) {
             TLCSpiderLoggerUtil.getLogger().error(ExceptionUtils.getFullStackTrace(e));
